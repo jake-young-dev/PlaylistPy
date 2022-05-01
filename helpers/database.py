b3153b51f50347742817fc2884cbaae2c66198bf
unknown <17075259+jake-young-dev@users.noreply.github.com>
2022-05-01 14:53:08 -0400

    testing for new db fields

--- helpers/database.py
+++ helpers/database.py
@@ -3,5 +3,5 @@ from pymongo import MongoClient
 def test():
     client = MongoClient('mongodb://localhost:27017/')
     db = client.web_db
-    print(db)
+    print(db.playlists.find({}))
     client.close()
